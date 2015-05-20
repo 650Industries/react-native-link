d26cf79cad232e0a257b74884cdee18c016decee
Charlie Cheever <ccheever@gmail.com>
2015-05-19 23:30:51 -0700

    Initial add

---
+++ link.js
@@ -0,0 +1,36 @@
+/**
+ * A trivial example of a React Native application
+ */
+var React = require('react-native');
+
+var {
+  AppRegistry,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var URLHandler = require('react-native-url-handler');
+
+class Link extends React.Component {
+  render() {
+    return (
+      <View
+        onPress={this._linkPressed}
+      >
+        {this.props.children}
+      </View>
+    );
+  }
+
+  _linkPressed() {
+    if (this.props.source && this.props.source.uri) {
+      var url = this.props.source.uri;
+      URLHandler.openURL(url);
+    }
+  }
+}
+
+module.exports = Link;
