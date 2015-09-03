bc6ee59daa65db2f6ba3c3b0689816172074ca2c
Charlie Cheever <ccheever@gmail.com>
2015-09-03 01:53:57 -0700

    OK finally fixed bugs so that this works with the latest versions of stuff

--- link.js
+++ link.js
@@ -9,33 +9,29 @@ var {
   ScrollView,
   StyleSheet,
   Text,
+  TouchableHighlight,
   View,
 } = React;
 
-var URLHandler = require('react-native-url-handler');
-
-let openUrl = URLHandler.openUrl || URLHandler.openURL || null;
-if (!openUrl) {
-  throw new Error("react-native-link can't figure out how to open links with this version of react-native-url-handler! :(");
-}
+let URLHandler = require('react-native-url-handler');
 
 class Link extends React.Component {
   render() {
     return (
-      <View
-        onPress={this._linkPressed}
+      <TouchableHighlight {...this.props}
+        onPress={() => {
+          console.log("linkPressed", this.props.source);
+          if (this.props.source && this.props.source.uri) {
+            let url_ = this.props.source.uri;
+            URLHandler.openUrl(url_);
+          }
+        }}
       >
         {this.props.children}
-      </View>
+      </TouchableHighlight>
     );
   }
 
-  _linkPressed() {
-    if (this.props.source && this.props.source.uri) {
-      let url_ = this.props.source.uri;
-      openUrl(url_);
-    }
-  }
 }
 
 module.exports = Link;
